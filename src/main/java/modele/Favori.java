5f0c292f10234d62b065290a2273b5cdedb620bd
code21112 <code21112@protonmail.com>
2022-07-11 10:34:25 +0200

    Changed ids to objects within Class Favori

--- src/main/java/modele/Favori.java
+++ src/main/java/modele/Favori.java
@@ -2,38 +2,45 @@ package modele;
 
 public class Favori {
 private int id_favori;
-private int id_produit;
-private int id_utilisateur;
+private Produit produit;
+private Utilisateur utilisateur;
 
 public Favori() {
 
 }
-public Favori(int id_favori, int id_produit, int id_utilisateur) {
+
+public Favori(int id_favori, Produit produit, Utilisateur utilisateur) {
+	super();
 	this.id_favori = id_favori;
-	this.id_produit = id_produit;
-	this.id_utilisateur = id_utilisateur;
+	this.produit = produit;
+	this.utilisateur = utilisateur;
 }
+
+public Produit getProduit() {
+	return produit;
+}
+
+public void setProduit(Produit produit) {
+	this.produit = produit;
+}
+
+public Utilisateur getUtilisateur() {
+	return utilisateur;
+}
+
+public void setUtilisateur(Utilisateur utilisateur) {
+	this.utilisateur = utilisateur;
+}
+
 public int getId_favori() {
 	return id_favori;
 }
 public void setId_favori(int id_favori) {
 	this.id_favori = id_favori;
 }
-public int getId_produit() {
-	return id_produit;
-}
-public void setId_produit(int id_produit) {
-	this.id_produit = id_produit;
-}
-public int getId_utilisateur() {
-	return id_utilisateur;
-}
-public void setId_utilisateur(int id_utilisateur) {
-	this.id_utilisateur = id_utilisateur;
-}
+
 @Override
 public String toString() {
-	return "Favori [id_favori=" + id_favori + ", id_produit=" + id_produit + ", id_utilisateur=" + id_utilisateur + "]";
+	return "Favori [id_favori=" + id_favori + ", utilisateur=" + utilisateur + "]";
 }
-
 }
